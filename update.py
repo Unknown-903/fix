de8f514da2a12791b4c804b0965424d24eab4b60
Unknown-903 <shubhamsinghal05072008@gmail.com>
2024-12-16 09:41:19 +0530

    Update update.py

--- update.py
+++ update.py
@@ -27,7 +27,7 @@ try:
 except:
     pass
 
-BOT_TOKEN = environ.get('BOT_TOKEN', '7227008403:AAHUERA7q_dGnTcm-s5RQ0Q3s0P4iEjhIBE')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7621829701:AAHZo5X_rXV0ESKVFqR8P5PyvobnaYXZrzc')
 if len(BOT_TOKEN) == 0:
     log_error("BOT_TOKEN variable is missing! Exiting now")
     exit(1)
